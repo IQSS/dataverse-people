c5a44681d2aabf7cd27901ceee430f3ec571b4dd
Philip Durbin <philip_durbin@harvard.edu>
2023-05-03 21:17:54 -0400

    reformat with mvn spotless:apply

--- backend/src/main/java/org/dataverse/people/JAXRSConfiguration.java
+++ backend/src/main/java/org/dataverse/people/JAXRSConfiguration.java
@@ -3,9 +3,6 @@ package org.dataverse.people;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/**
- * Configures a JAX-RS endpoint.
- */
+/** Configures a JAX-RS endpoint. */
 @ApplicationPath("api/v1")
-public class JAXRSConfiguration extends Application {
-}
+public class JAXRSConfiguration extends Application {}
